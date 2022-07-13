63901f832e174130f5ccc9b85fa4e0d4700bda89
Sandra Tena <sandratena06@gmail.com>
2022-07-13 13:29:03 -0700

    create initial files
    Co-authored-by: Daniel Ware <waredanielb@gmail.com>
    Co-authored-by: Anthony DiFalco <aodifalco@gmail.com>

---
+++ RockPaperScissors.Tests/ModelTests/RPSTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RockPaperScissors.Models;
+
+namespace RockPaperScissors.Tests
+{
+  [TestClass]
+  public class ItemTests
+  {
+
+    // Test methods will go here.
+
+  }
+}
